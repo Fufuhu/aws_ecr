f6786309cd62eb48606b48e52033024b802e24c4
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2023-07-04 22:27:19 +0900

    update thumbprint due to multiple thumbprint

--- modules/iam/provider/oidc/github/oidc_provider.tf
+++ modules/iam/provider/oidc/github/oidc_provider.tf
@@ -1,18 +1,5 @@
 locals {
   domain = "https://token.actions.githubusercontent.com"
-  // OpenIDプロバイダの設定ドキュメントパス( OpenID Connect Discovery 1.0 )
-  // ref) https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderConfigurationRequest
-  openid_provider_configuration_path = "/.well-known/openid-configuration"
-  // OpenIDプロバイダ設定の取得URL
-  openid_configuration_url = "${local.domain}${local.openid_provider_configuration_path}"
-}
-
-data "http" "openid_configuration" {
-  url = local.openid_configuration_url
-}
-
-data "tls_certificate" "encryption_key" {
-  url = jsondecode(data.http.openid_configuration.response_body).jwks_uri
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
@@ -21,6 +8,7 @@ resource "aws_iam_openid_connect_provider" "github" {
     "sts.amazonaws.com"
   ]
   thumbprint_list = [
-    data.tls_certificate.encryption_key.certificates[0].sha1_fingerprint
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 }
